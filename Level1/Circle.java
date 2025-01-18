de3c9ece629cd4b62a7d5f9772eecd89e61dfc78
2002prath <prathammishra1111@gmail.com>
2025-01-18 12:41:22 +0530

    Area of circle code

---
+++ Level1/Circle.java
@@ -0,0 +1,16 @@
+public class Circle {
+    public static void main(String[] mmg)
+    { 
+       Area a1 = new Area(); 
+       a1.display(4.33); 
+        
+    }
+} 
+class Area
+{ 
+    
+    public void display(double radius)
+    { 
+         System.out.print(3.14*radius*radius); 
+    }
+}
